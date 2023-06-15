4f879d7b228b65b207d098b991d51778f727baf3
Kento-99 <is0713pf@ed.ritsumei.ac.jp>
2023-06-15 15:22:51 +0900

    定期実行

--- main.py
+++ main.py
@@ -1,19 +1,29 @@
 from linebot import LineBotApi
 from linebot.models import TextSendMessage
 import configparser
+import schedule
+import time
 
-#ConfigParserオブジェクトを生成
-config = configparser.ConfigParser()
+def task():
+ #ConfigParserオブジェクトを生成
+ config = configparser.ConfigParser()
+ 
+ #設定ファイル読み込み
+ config.read('config.ini')
+ 
+ CHANNEL_ACCESS_TOKEN = config['DEFAULT']['CHANNEL_ACCESS_TOKEN'] #ここに自分のトークンを入れて下さい
+ 
+ USER_ID = config['DEFAULT']['USER_ID'] #ここに自分のユーザーIDを入れて下さい
+ 
+ text = "hi"
+ 
+ line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
+ 
+ line_bot_api.push_message(USER_ID, TextSendMessage(text=text))
 
-#設定ファイル読み込み
-config.read('config.ini')
+schedule.every(10).seconds.do(task)  # 関数taskを、10秒毎に実行する
 
-CHANNEL_ACCESS_TOKEN = config['DEFAULT']['CHANNEL_ACCESS_TOKEN'] #ここに自分のトークンを入れて下さい
+while True:
+ schedule.run_pending()
+ time.sleep(1)
 
-USER_ID = config['DEFAULT']['USER_ID'] #ここに自分のユーザーIDを入れて下さい
-
-text = "hi"
-
-line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
-
-line_bot_api.push_message(USER_ID, TextSendMessage(text=text))
