ec678fd3662884ca688636a1e5a610d1307f8deb
Kento-99 <is0713pf@ed.ritsumei.ac.jp>
2023-06-24 06:18:03 +0900

    main

--- main.py
+++ main.py
@@ -1,8 +1,4 @@
-import chromedriver_binary
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -18,9 +14,11 @@ config = configparser.ConfigParser()
 #設定ファイル読み込み
 config.read('config.ini')
 
+#linebotの送信前準備
 CHANNEL_ACCESS_TOKEN = config['DEFAULT']['CHANNEL_ACCESS_TOKEN'] 
-
 USER_ID = config['DEFAULT']['USER_ID'] 
+
+line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 
 # ログインに使用するユーザー名とパスワード
 username =config['DEFAULT']['username']
@@ -89,7 +87,7 @@ for tr_tag in soup1.find_all('tr'):
     minitest_data = [td.text for td in td_tags]
     if len(minitest_data) > 2 and minitest_data[2] != '期限' and minitest_data[2] != '':
      deadline = datetime.datetime.strptime(minitest_data[2], '%Y-%m-%d %H:%M').date()
-     if deadline - today == datetime.timedelta(days=1):
+     if datetime.timedelta(days=0) <= (deadline - today) <= datetime.timedelta(days=1):
       kadai_array1.append(minitest_data)
 
 
@@ -114,7 +112,7 @@ for tr_tag in soup2.find_all('tr'):
     questionary_data = [td.text for td in td_tags]
     if len(questionary_data) > 2 and questionary_data[2] != '期限' and questionary_data[2] != '':
      deadline = datetime.datetime.strptime(questionary_data[2], '%Y-%m-%d %H:%M').date()
-     if deadline - today == datetime.timedelta(days=1):
+     if datetime.timedelta(days=0) <= (deadline - today) <= datetime.timedelta(days=1):
       kadai_array2.append(questionary_data)
 
 
@@ -139,21 +137,23 @@ for tr_tag in soup3.find_all('tr'):
     report_data = [td.text for td in td_tags]
     if len(report_data) > 2 and report_data[2] != '期限' and report_data[2] != '':
      deadline = datetime.datetime.strptime(report_data[2], '%Y-%m-%d %H:%M').date()
-     if deadline - today == datetime.timedelta(days=1): #課題の期限を読み取り、一日以内の場合kadai_arrayに追加する
+     if datetime.timedelta(days=0) <= (deadline - today) <= datetime.timedelta(days=1): #課題の期限を読み取り、一日以内の場合kadai_arrayに追加する
       kadai_array3.append(report_data)
 
 
 # 結果を出力
-for data_row1 in kadai_array1:
+if kadai_array1:
+ for data_row1 in kadai_array1:
     minitest_message = f'{data_row1[1]}\n {data_row1[0].strip()}\nの提出期限が迫っています。'
-    line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
     line_bot_api.push_message(USER_ID, TextSendMessage(text=minitest_message))
-for data_row2 in kadai_array2:
+if kadai_array2:
+ for data_row2 in kadai_array2:
     questionary_message = f'{data_row2[1]}\n {data_row2[0].strip()}\nの提出期限が迫っています。'
-    line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
     line_bot_api.push_message(USER_ID, TextSendMessage(text=questionary_message))
-for data_row3 in kadai_array3:
+if kadai_array3:
+ for data_row3 in kadai_array3:
     report_message = f'{data_row3[1]}\n {data_row3[0].strip()}\nの提出期限が迫っています 。'
-    line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
     line_bot_api.push_message(USER_ID, TextSendMessage(text=report_message))
 
+#webdriverの終了処理
+driver.quit()
