e055dc825631a9bb49559989d904e2151047a925
Kento-99 <132346733+Kento-99@users.noreply.github.com>
2023-07-12 22:59:35 +0900

    Update main.py

--- main.py
+++ main.py
@@ -32,13 +32,6 @@ password =os.environ['password']
 
 # WebDriverでChromeブラウザを起動
 options = webdriver.ChromeOptions()
-options.add_argument('--headless')
-options.add_argument('--disable-dev-shm-usage')
-options.add_argument('start-maximized')
-options.add_argument('disable-infobars')
-options.add_argument('--disable-extensions')
-options.add_argument('--disable-gpu')
-options.add_argument('--no-sandbox')
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 # manabaのログインページを開く
