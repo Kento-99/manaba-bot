f7c2b8422d89433a92a20d77fa26ef94604d0b8b
Kento-99 <132346733+Kento-99@users.noreply.github.com>
2023-07-12 23:05:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -32,6 +32,10 @@ password =os.environ['password']
 
 # WebDriverでChromeブラウザを起動
 options = webdriver.ChromeOptions()
+options.add_argument('--headless')
+options.add_argument('--no-sandbox')
+options.add_argument('--disable-dev-shm-usage')
+options.add_argument('--remote-debugging-port=9222')
 driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
 
 # manabaのログインページを開く
